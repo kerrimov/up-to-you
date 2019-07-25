4c7ccc94a453fa0714c02065228a1754a738cda1
kerrimov <tolik.kerimov@gmail.com>
2019-07-25 12:28:06 +0300

    added template render using ts

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,15 @@ const path = require("path");
 
 module.exports = {
   entry: {
-    app: "./src/index.js"
+    app: "./src/index.ts"
   },
   output: {
     filename: "[name].js",
     path: path.resolve(__dirname, "./dist"),
     publicPath: "/dist"
+  },
+  resolve: {
+    extensions: [".jsx", ".ts", ".tsx", ".js"]
   },
   module: {
     rules: [
@@ -24,6 +27,21 @@ module.exports = {
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: "html-loader"
+        }
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {}
+          }
+        ]
       }
     ]
   },
