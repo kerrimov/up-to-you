afa0f01aefe1cba2abf9c08dd8f666a759f35c5b
Sergey <sergeyserg.sms@gmail.com>
2019-07-25 17:49:07 +0300

    for remote git

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+import "./ts/script.ts";
+import "./css/style.css";
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    app: "./src/index.ts"
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "/dist"
+  },
+  resolve: {
+    extensions: [".jsx", ".ts", ".tsx", ".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: "/node_modules/"
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        loader: "babel-loader",
+        exclude: "/node_modules/"
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: "html-loader"
+        }
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {}
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    overlay: true
+  }
+};
