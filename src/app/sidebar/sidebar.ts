0082bae3edc68a342d94157d8a98e8ba6cad4f56
Sergey <sergeyserg.sms@gmail.com>
2019-07-28 00:57:51 +0300

    improve clickable area and fix layout

--- src/app/sidebar/sidebar.ts
+++ src/app/sidebar/sidebar.ts
@@ -3,10 +3,12 @@ import { Mountable } from "../interfaces/Mountable";
 export class Sidebar implements Mountable {
   private sidebar: HTMLElement;
   private closeButton: HTMLElement;
-  constructor(){
-    this.sidebar = document.getElementById("sidebar");
+
+  constructor() {
+    this.sidebar = document.getElementById("sidebar"); //has to be empty template of sidebar
     this.closeButton = document.getElementById("sidebar__closebtn");
   }
+
   mount() {
     if (document.readyState == "loading") {
       document.addEventListener("DOMContentLoaded", () => this.init());
@@ -14,25 +16,44 @@ export class Sidebar implements Mountable {
       this.init();
     }
   }
+
   private init() {
-      const statusContainer: HTMLElement = document.querySelector(
+    /*const statusContainer: HTMLElement = document.querySelector(
       ".status-container"
     );
-    statusContainer.addEventListener("click", evt => this.open(evt));
+    statusContainer.addEventListener("click", evt => this.open(evt));*/
+    document.body.addEventListener("click", evt => this.open(evt));
   }
-  private open(evt) {
-      let target = evt.target;
-      //should be card tag to find
-      if (target.closest(".module")) {
-      this.findCard();
-      this.sidebar.classList.remove("sidebar--hidden");
 
-      this.closeButton.addEventListener("click", evt => this.close(evt));
-    } else return;
+  private open(evt) {
+    let target = evt.target;
+
+    if (target.closest(".card")) {
+      this.showCard();
+
+      /*const cardContainer = this.closeButton.addEventListener("click", evt =>
+        this.close(evt)
+      );*/
+    } else this.close(evt);
   }
+
   private close(evt) {
-      this.sidebar.classList.add("sidebar--hidden");
+    let target = evt.target;
+
+    if (
+      (target.closest(".sidebar") && !target.matches(".sidebar__closebtn")) ||
+      target.matches(".card__container")
+    ) {
+      return;
+    } else this.sidebar.classList.add("sidebar--hidden");
   }
+
+  private showCard() {
+    this.findCard();
+
+    this.sidebar.classList.remove("sidebar--hidden");
+  }
+
   private findCard() {
     // alert("findCard");
   }
