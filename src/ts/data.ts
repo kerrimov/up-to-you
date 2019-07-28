e6656e70a4bfbd7063bd42582dbeec33bca4d5dc
kerrimov <tolik.kerimov@gmail.com>
2019-07-28 16:38:02 +0300

    demo JSON load added

--- src/ts/data.ts
+++ src/ts/data.ts
@@ -1,5 +1,5 @@
-export async function fetchAsync () {
-  let response = await fetch('../cards.json');
+export async function fetchAsync() {
+  let response = await fetch("../cards2.json");
   let data = await response.json();
   return data;
 }
--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,4 +1,13 @@
 import { App } from "../app/app";
+import { Card } from "../app/card/card";
+import { Module } from "../app/module/module";
+import { fetchAsync } from "../ts/data";
+
+const data = fetchAsync();
 
 const app = new App();
 app.mount();
+const card = new Card();
+card.mount(data);
+const module = new Module();
+module.mount(data);
