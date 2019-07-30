dfd6cbb7611544522357234bce36749769f67c7b
maria.shpura <maria.shpura@sheerchain.com>
2019-07-30 11:56:27 +0300

    Change render and add scroll

--- src/ts/render.ts
+++ src/ts/render.ts
@@ -1,65 +1,23 @@
-export function Render(data) {
+import card from "../app/card/card.html"
+import { fetchAsync } from "./data";
+
+const data = fetchAsync();
+
+export function render(status) {
+  let count = 0;
   data.then(response => {
-    response.map(item => {
-      item.modules.map(element => {
-        for (let key in element.moduleStatus) {
-          if (element.moduleStatus.key == "not_done") {
-            const cardContainerNotDone = document.getElementById(
-              "card__container--not_done"
-            );
-            cardContainerNotDone.innerHTML += `<div class="card">
-              <div class="card__header"><h4>${item.courseTitle}</h4></div>
-              <div class="module__container">
-              <div class="module"><p>${element.moduleTitle}</p></div>
-              </div>`;
-          } else if (element.moduleStatus.key == "in_progress") {
-            const cardContainerInProgress = document.getElementById(
-              "card__container--in_progress"
-            );
-            cardContainerInProgress.innerHTML += `<div class="card">
-              <div class="card__header"><h4>${item.courseTitle}</h4></div>
-              <div class="module__container">
-              <div class="module"><p>${element.moduleTitle}</p></div>
-              </div>`;
-          } else if (element.moduleStatus.key == "suspended") {
-            const cardContainerSuspended = document.getElementById(
-              "card__container--suspended"
-            );
-            cardContainerSuspended.innerHTML += `<div class="card">
-              <div class="card__header"><h4>${item.courseTitle}</h4></div>
-              <div class="module__container">
-              <div class="module"><p>${element.moduleTitle}</p></div>
-              </div>`;
-          } else if (element.moduleStatus.key == "high_priority") {
-            const cardContainerHighPriority = document.getElementById(
-              "card__container--high_priority"
-            );
-            cardContainerHighPriority.innerHTML += `<div class="card">
-              <div class="card__header"><h4>${item.courseTitle}</h4></div>
-              <div class="module__container">
-              <div class="module"><p>${element.moduleTitle}</p></div>
-              </div>`;
-          } else if (element.moduleStatus.key == "rejected") {
-            const cardContainerRejected = document.getElementById(
-              "card__container--rejected"
-            );
-            cardContainerRejected.innerHTML += `<div class="card">
-              <div class="card__header"><h4>${item.courseTitle}</h4></div>
-              <div class="module__container">
-              <div class="module"><p>${element.moduleTitle}</p></div>
-              </div>`;
-          } else if (element.moduleStatus.key == "done") {
-            const cardContainerDone = document.getElementById(
-              "card__container--done"
-            );
-            cardContainerDone.innerHTML += `<div class="card">
-              <div class="card__header"><h4>${item.courseTitle}</h4></div>
-              <div class="module__container">
-              <div class="module"><p>${element.moduleTitle}</p></div>
-              </div>`;
-          } else {
-            console.log("Something wrong with JSON data");
-          }
+    response.forEach((item) => {
+      item.modules.forEach((element) => {
+        if (element.moduleStatus.key === status) {
+          let cardContainer = document.getElementById("card__container--" + status);
+          cardContainer.innerHTML += `${card}`;
+          let courseTitle = cardContainer.querySelectorAll(".card__header")[count];
+          let courseId = cardContainer.querySelectorAll(".card")[count];
+          let moduleTitle = cardContainer.querySelectorAll(".module")[count];
+          courseId.setAttribute("id", item.guid);
+          courseTitle.textContent = item.courseTitle;
+          moduleTitle.textContent = element.moduleTitle;
+          count++;
         }
       });
     });
