b0c0bbb8684fea4c711213a3ee661ef17c5233c7
Sergey <sergeyserg.sms@gmail.com>
2019-07-31 03:19:36 +0300

    found solution for quick insert(needs to be finished)

--- src/app/card/card.ts
+++ src/app/card/card.ts
@@ -4,11 +4,11 @@ import card from "./card.html";
 export class Card {
   mount(data) {
     data.then(response => {
-      response.map(item => {
+      response.forEach(item => {
         const cardContainer = document.getElementById("card__container");
         cardContainer.innerHTML += `<div class="card" id="card">
-                                    <div class="card__header"><h4>${item.courseTitle}</h4></div>
-                                    <div class="module__container" id="module__container"></div>
+                                      <div class="card__header"><h4>${item.courseTitle}</h4></div>
+                                      <div class="module__container" id="module__container"></div>
                                     </div>`;
       });
     });
--- src/app/sidebar/sidebar.ts
+++ src/app/sidebar/sidebar.ts
@@ -8,6 +8,11 @@ import moduleTmpl from "../card/module.html";
 let baseURI: string = "http://localhost:8080/";
 let path: string = "cards2.json";
 
+function createFragmentFromString(str){
+  var template = document.createElement("template");
+  template.innerHTML = str;
+  return template.content;
+}
 function getHtmlNodeFromString(templateHhtmlString: string) {
   const serviceElement: HTMLElement = document.createElement("div");
   serviceElement.innerHTML = templateHhtmlString;
@@ -23,11 +28,16 @@ export class Sidebar implements Mountable {
   private moduleSingleTmpl: Node;
   private closeButton: HTMLElement;
   private isOpen: boolean;
+  private oldSb: Element;
+  private sbTmplTag: HTMLElement;
 
   constructor() {
     // this.sidebarTmplTag = document.getElementById("tmpl-sidebar").content.cloneNode(true);//
     //sidebar has to be empty template of html for cloning everytime it needs to be filled
     this.isOpen = false;
+    this.oldSb = null;
+    this.sbTmplTag = document.getElementById("tmpl-sidebar");
+
     this.sidebarTmplNode = getHtmlNodeFromString(sidebarTmpl);
     this.moduleContTmpl = getHtmlNodeFromString(moduleTmpl);
     this.moduleSingleTmpl = this.moduleContTmpl.firstChild;
@@ -51,12 +61,12 @@ export class Sidebar implements Mountable {
   private open(evt) {
     let target = evt.target;
     // console.dir(target);
-    debugger;
+    // debugger;
     if (target.closest(".card")) {
       // debugger;
-      const idCard = target.id;
+      const idCard = target.closest(".card").id;
       const cardName = target.closest(".card").querySelector("h4").innerText; //will be deleted
-      this.showCard(cardName); //will be idCard - param
+      this.render(cardName, idCard); //will be idCard - param
     } else if (!this.isOpen) {
       return;
     } else this.close(evt);
@@ -79,18 +89,73 @@ export class Sidebar implements Mountable {
     }
   }
 
-  private showCard(name: string, id?: number) {
-    this.sidebarTmplTag = document.getElementById("tmpl-sidebar").content.cloneNode(true);
-    this.sidebarTmplTag.querySelector("h3").textContent = name;
-    this.sidebar.appendChild(this.sidebarTmplTag);
+  private render(name: string, id?: number) {
+    debugger;
+    const t1 = `                <div class="sidebar__header">
+                                  <h3 class="sidebar__h3">Courses</h3>
+                                  <span class="sidebar__closebtn">&times</span>
+                                </div>
+                                <div class="module__container">`;
+    const t2 = `<div class="module">
+                                          <p><em class="module_status"></em></p>
+                                      </div>`;
+    const t3 = `</div>`;
+    const total = t1+t2+t2+t2+t3;
+    const tmplFirstPart = `${t1}`;
+    const tmplModuleAndStatusPart = `${t2}`;
+    const tmplLastPart = `${t3}`;
+
+    let fragment = createFragmentFromString(total);
+    // fragment.appendChild(createFragmentFromString(moduleTmpl));
+
+    this.sidebar.appendChild(fragment);
+debugger;
+    let templateNewTag = document.createElement("template");
+    templateNewTag.id = "tmpl-sidebar";
+
+    templateNewTag.insertAdjacentHTML("beforeend", total);
+
+    /*templateNewTag.insertAdjacentHTML("beforeend", tmplFirstPart);
+    templateNewTag.insertAdjacentHTML("beforeend", tmplModuleAndStatusPart);
+    templateNewTag.insertAdjacentHTML("beforeend", tmplModuleAndStatusPart);
+    templateNewTag.insertAdjacentHTML("beforeend", tmplModuleAndStatusPart);
+    templateNewTag.insertAdjacentHTML("beforeend", tmplLastPart);*/
+
+    this.sidebar.appendChild(templateNewTag);
+
+    let newFilledTmpl = document.querySelector("#tmpl-sidebar");
+    let modStat = newFilledTmpl.querySelectorAll("em");
+    modStat[0].textContent = "something1";
+    modStat[1].textContent = "something2";
+    modStat[2].textContent = "something3";
+    newFilledTmpl.querySelector(".module__container").appendChild(modStat[0]);
+
+    let newCloneTmpl = document.importNode(newFilledTmpl.content, true);
+    // this.sidebar.innerHTML = "";
+    // this.sidebar.appendChild(newCloneTmpl);
+
+    this.sidebar.appendChild(newFilledTmpl.content);
+
+    // let newCloneTmpl = newFilledTmpl.content.cloneNode(true);
+    // this.sidebar.insertBefore(newCloneTmpl, this.sidebar.lastElementChild);
+
+
+   /* let headingSb = this.sbTmplTag.content.querySelector("h3");
+    headingSb.textContent = name;
+    /!*let newModule = this.sbTmplTag.content.querySelector(".module__container");
+    newModule.appendChild(headingSb);*!/
+
+    let cloneSbTmplTag = this.sbTmplTag.content.cloneNode( true);
+
+    //to remove previous insertion, but not first
+    if(this.oldSb){
+      this.oldSb = this.sidebar.removeChild(this.sidebar.lastElementChild);
+    }
+    this.sidebar.appendChild(cloneSbTmplTag);
+    this.oldSb = this.sidebar.lastElementChild;*/
+
     // this.findCard(name);
 
-    /*let sidebarHeader = this.sidebarTmplNode.getRootNode().querySelector("h3");
-    console.log(sidebarHeader);
-    sidebarHeader.textContent = name;*/
-
-    /*this.sidebar.replaceWith(this.sidebarTmplNode);
-    this.sidebar = document.querySelector(".sidebar");*/ //new sidebar
     this.sidebar.classList.remove("sidebar--hidden");
     this.isOpen = true;
   }
